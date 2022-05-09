062f33e8676a9e128b481e1fbe1fd2bab77b2a5a
Park21700305 <93187535+Park21700305@users.noreply.github.com>
2022-05-09 23:36:13 +0900

    수정

--- system.h
+++ system.h
@@ -4,7 +4,9 @@ typedef struct{ // 디저트 구조체
 char dc; // 디저트의 대분류(drink or cake)
 char name[30]; // 디저트의 이름
 int size; // 음료 디저트 사이즈 (Tall, Grande, Venti)
-int cost; // 디저트 가격
+int cost1;
+int cost2;
+int cost3; // 디저트 가격
 int togo; // 포장유무 (1(yes) or 2(no))
 int coldHot; // 온도(1(ice) or 2(hot))
 } Dessert;
